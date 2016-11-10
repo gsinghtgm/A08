5f18787dfd31a7f87eec0c892aaf9654451367f1
Gurparkash <Gurparkash@10.0.104.74>
2016-11-10 13:43:06 +0100

    Dokumentation vervollständigt.

--- src/gruppe2/Flaeche.java
+++ src/gruppe2/Flaeche.java
@@ -1,16 +1,38 @@
 package gruppe2;
 
 public class Flaeche implements IFlaeche {
-	double flaeche;
+	/**
+	 * @author Gurparkash Singh
+	 * @since 10-11-2016
+	 */
+	private double flaeche;
 
+	/**
+	 * Konstruktor für Dreieckfläche.
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 */
 	public Flaeche(double a, double b, double c) {
 		flaeche = Math.sqrt(((a + b + c) * (a + b - c) * (b + c - a) * (c + a - b)) / 4);
 	}
 
+	/**
+	 * Konstruktor für Rechteckfläche.
+	 * 
+	 * @param a
+	 * @param b
+	 */
 	public Flaeche(double a, double b) {
 		flaeche = a * b;
 	}
 
+	/**
+	 * Konstruktor für Kreisfläche.
+	 * 
+	 * @param a
+	 */
 	public Flaeche(double a) {
 		flaeche = a * a * Math.PI;
 	}
