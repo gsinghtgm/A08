952e7cf599a1a30fe85bc1de3d94383271b72017
Mohamed Elshal <melshal@student.tgm.ac.at>
2016-11-10 13:41:25 +0100

    Überarbeitete Zahlenklasse

--- src/gruppe2/Zahlen.java
+++ src/gruppe2/Zahlen.java
@@ -1,20 +1,18 @@
 package gruppe2;
 
 public class Zahlen implements IZahlen {
-	private double minimum;
-	private double maximum;
+	private double minimum = 100;
+	private double maximum = -100;
 	private double summe;
-	
-	public Zahlen(double zahl1, double zahl2){
-		if(zahl1 < zahl2){
-			this.minimum =zahl1;
-			this.maximum = zahl2;
-		}else{
-			this.minimum = zahl2;
-			this.maximum = zahl1;
-		}
-		
+
+	public Zahlen(double min, double max) {
+		if (min < minimum)
+			this.minimum = min;
+
+		if (max > maximum)
+			this.maximum = max;
 	}
+
 	@Override
 	public double getSumme() {
 		return summe;
@@ -32,7 +30,7 @@ public class Zahlen implements IZahlen {
 
 	@Override
 	public void add(double value) {
-		double s = summe+ value;
+		summe += value;
+		new Zahlen(value, value);
 	}
-
 }
