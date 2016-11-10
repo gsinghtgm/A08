daaa8731842c7568f951590509353443bb817400
Mirza Kurtalic <mkurtalic@student.tgm.ac.at>
2016-11-10 13:57:32 +0100

    fertig

--- src/gruppe2/Rauminhalt.java
+++ src/gruppe2/Rauminhalt.java
@@ -1,36 +1,40 @@
 package gruppe2;
 
-public class rauminhalt implements IRaumInhalt {
+public class Rauminhalt implements IRaumInhalt {
 	private double rauminhalt;
 	/**
-	 * zylinder
+	 * Hier wird das Volumen vom Zylinder berechnet
+	 * @author Mirza Kurtalic
 	 * @param radius
 	 * @param höhe
+	 * @version 10.11
 	 */
-	public rauminhalt(double radius, double hoehe) {
+	public Rauminhalt(double radius, double hoehe) {
 		Flaeche f = new Flaeche(radius);
 		rauminhalt=f.getFlaeche()*hoehe;
 	}
 
 	/**
-	 * quader
+	 * Hier wird das Volumen vom Quader berechnet
 	 * @param länge
 	 * @param breite
 	 * @param höhe
+	 * @since 10.11
 	 */
-	public rauminhalt(double laenge, double breite, double hoehe) {
+	public Rauminhalt(double laenge, double breite, double hoehe) {
 		Flaeche f = new Flaeche(laenge,breite);
 		rauminhalt = f.getFlaeche()*hoehe;
 	}
 
 	/**
-	 * 
+	 * Hier wird das Volumen vom Prisma berechnet
 	 * @param höhe
 	 * @param a
 	 * @param b
 	 * @param c
+	 * @since 10.11
 	 */
-	public rauminhalt(double hoehe, double a, double b, double c) {
+	public Rauminhalt(double hoehe, double a, double b, double c) {
 		Flaeche f = new Flaeche(a,b,c);
 		rauminhalt = f.getFlaeche()*hoehe;
 	}
