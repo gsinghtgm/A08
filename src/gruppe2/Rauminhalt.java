0738d342f7c2fb6a7267a3295c212d42aa96e7d7
Gurparkash <Gurparkash@10.0.104.74>
2016-11-10 13:59:51 +0100

    test

--- src/gruppe2/Rauminhalt.java
+++ src/gruppe2/Rauminhalt.java
@@ -1,13 +1,13 @@
 package gruppe2;
 
-public class rauminhalt implements IRaumInhalt {
+public class Rauminhalt implements IRaumInhalt {
 	private double rauminhalt;
 	/**
 	 * zylinder
 	 * @param radius
 	 * @param höhe
 	 */
-	public rauminhalt(double radius, double hoehe) {
+	public Rauminhalt(double radius, double hoehe) {
 		Flaeche f = new Flaeche(radius);
 		rauminhalt=f.getFlaeche()*hoehe;
 	}
@@ -18,7 +18,7 @@ public class rauminhalt implements IRaumInhalt {
 	 * @param breite
 	 * @param höhe
 	 */
-	public rauminhalt(double laenge, double breite, double hoehe) {
+	public Rauminhalt(double laenge, double breite, double hoehe) {
 		Flaeche f = new Flaeche(laenge,breite);
 		rauminhalt = f.getFlaeche()*hoehe;
 	}
@@ -30,7 +30,7 @@ public class rauminhalt implements IRaumInhalt {
 	 * @param b
 	 * @param c
 	 */
-	public rauminhalt(double hoehe, double a, double b, double c) {
+	public Rauminhalt(double hoehe, double a, double b, double c) {
 		Flaeche f = new Flaeche(a,b,c);
 		rauminhalt = f.getFlaeche()*hoehe;
 	}
